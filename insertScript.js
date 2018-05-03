e84544ba81ae933791221231cd4b5edff14acc66
Antoni <antonig@sas.upenn.edu>
2018-05-02 20:38:57 -0400

    cleanup

--- insertScript.js
+++ insertScript.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
 	c = $('.commentarea :nth-child(4)');
 	d = c.find('.comment');
 	$(document).keypress(function(e) {
-		console.log(e);
 		var tag = e.target.tagName.toLowerCase();
         if ( e.which === 113 && tag != 'input' && tag != 'textarea') {
        		d.first().remove();
